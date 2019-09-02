1d03d36c2cd8c9220e4d1caa7a8ea60cc1130cff
itsgoodtobegrant <itsgoodtobegrant@gmail.com>
2019-09-02 09:25:42 -0700

    Working Gifs animated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,3 +13,37 @@ function movieButtonClicked() {
         $('#movie-buttons').append("<button type='button' onclick='searchGif(\"" + userInput + "\")' class='btn btn-success' value=' " + userInput + "'>" + userInput + "</button>");
     }
 }
+
+function searchGif(gifName) {
+    $.ajax({
+        url: 'https://api.giphy.com/v1/gifs/search?q= ' + gifName + ' &api_key=LzFwoZvUsteMC912e7ifoGYF8kv9dxSz',
+        type: 'GET',
+    })
+    .done(function(response) {
+        displayGif(response);
+    })
+}
+
+function displayGif(response) {
+    $('#movies').empty();
+    for (var i = 0; i < response.data.length; i++) {
+        var rating = "<div class='ratings'> Rating:  " + (response.data[i].rating) + " </div>";
+        var image = rating + '<img src= " ' + response.data[i].images.fixed_height_still.url +
+        '" data-still=" ' + response.data[i].images.fixed_height_still.url +
+        ' " data-animate=" ' + response.data[i].images.fixed_height.url + '"data-state="still" class="movImage" style= "width:250px">';
+
+        image = '<div class="col-md-4">' + image + "</div>";
+        $("#movies").append(image);
+    }
+
+    $('.movImage').on('click', function() {
+        var state = $(this).attr('data-state');
+        if (state == 'still') {
+            $(this).attr('src', $(this).attr("data-animate"));
+            $(this).attr('data-state', 'animate');
+        } else {
+            $(this).attr('src', $(this).attr("data-still"));
+            $(this).attr('data-state', 'still');
+        }
+    });
+}
